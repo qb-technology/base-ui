b10b8cab224f0c39b70c22aa27b71f94644ab17d
Cybandy <andyboat75@gmail.com>
2024-09-20 04:33:14 +0200

    added zod, pinia, @iconify/fluent

--- eslint.config.js
+++ eslint.config.js
@@ -5,5 +5,7 @@ export default withNuxt(
 ).overrideRules({
     'vue/block-tag-newline': 'off',
     '@stylistic/semi': 'off',
-    '@typescript-eslint/no-unused-vars': 'off'
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@stylistic/comma-dangle': 'off',
+    '@typescript-eslint/no-explicit-any': 'off'
 })
