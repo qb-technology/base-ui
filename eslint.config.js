e82d55318997adc37386981e7ed414eb86182ad2
Cybandy <andyboat75@gmail.com>
2024-09-22 16:18:17 +0200

    improved eslint config, added tailwind merge

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,12 @@
 import withNuxt from './.playground/.nuxt/eslint.config.mjs'
 
 export default withNuxt(
-    
+
 ).overrideRules({
-    'vue/block-tag-newline': 'off',
-    '@stylistic/semi': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
-    '@stylistic/comma-dangle': 'off',
-    '@typescript-eslint/no-explicit-any': 'off'
+  'vue/block-tag-newline': 'off',
+  '@stylistic/semi': 'off',
+  '@typescript-eslint/no-unused-vars': 'off',
+  '@stylistic/comma-dangle': 'off',
+  '@typescript-eslint/no-explicit-any': 'off',
+  'vue/html-self-closing': 'off'
 })
