bd242ee16dfaed971967a5d1eef9dd734595e6c2
Cybandy <andyboat75@gmail.com>
2024-10-20 21:33:21 +0200

    added some functions

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
-    "@nuxt/ui",
+    '@nuxt/ui',
     '@pinia/nuxt',
     '@pinia-plugin-persistedstate/nuxt',
     '@nuxt/scripts',
