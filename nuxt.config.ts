14d9ff966869dc88e47d35d568303cb15eea3d94
Cybandy <andyboat75@gmail.com>
2025-02-09 00:26:13 +0100

    enabling debugging options in nuxt conf

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  debug: process.env.NODE_ENV !== 'production',
+  sourcemap: process.env.NODE_ENV !== 'production',
   css: [
     './assets/css/main.css'
   ],
