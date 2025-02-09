fb30dab79caaf2abe8045c1d11aa0fe179d9109e
Cybandy <andyboat75@gmail.com>
2025-02-09 03:54:55 +0100

    still not working

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
   css: [
     './assets/css/main.css'
   ],
-  modules: ["@nuxt/ui", "@vueuse/nuxt"],
+  modules: ["@nuxt/ui", "@vueuse/nuxt", '@nuxt/eslint'],
   eslint: {
     config: {
       stylistic: {
