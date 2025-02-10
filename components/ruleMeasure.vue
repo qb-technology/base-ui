6d248e4f993f6946091c76d65ecdf01d9f1f81b7
Cybandy <andyboat75@gmail.com>
2025-02-10 02:25:42 +0100

    improved ruler

--- components/ruleMeasure.vue
+++ components/ruleMeasure.vue
@@ -19,7 +19,7 @@ local_modelValue.value = props.modelValue ? props.modelValue : local_modelValue.
 const slide_count = computed(() => Array.from(Array(props.length).keys()))
 
 const options: EmblaOptionsType = {
-  loop: false,
+  loop: true,
   axis: 'x',
   dragFree: true,
   watchSlides: false,
@@ -27,7 +27,7 @@ const options: EmblaOptionsType = {
 }
 
 if (props.modelValue) {
-  options.startIndex = props.modelValue
+  options.startIndex = props.modelValue - 1
 }
 
 const [emblaRef, emblaApi] = useEmblaCarousel(options)
@@ -38,6 +38,13 @@ const updateCurrentValue = (api: EmblaCarouselType) => {
   local_modelValue.value = api.selectedScrollSnap() + 1;
   emits('update:modelValue', local_modelValue.value)
 }
+
+// update index based on prop change
+const _mod = computed(() => props.modelValue)
+watch(_mod, () => {
+  if (!emblaApi.value || !_mod.value) return
+  emblaApi.value?.scrollTo(_mod.value - 1, false)
+})
 
 onMounted(() => {
   if (!emblaApi.value) return
@@ -51,13 +58,27 @@ watch(local_modelValue, () => {
 defineExpose({
   emblaRef, emblaApi
 })
+
+const embla_before_after = "before:bg-linear-to-r before:from-[var(--ui-bg)]/80 before:via-[var(--ui-bg)]/40 before:to-[var(--ui-bg)]/10 before:w-[calc(50%-7px)] before:h-full after:w-[calc(50%-7px)] after:bg-linear-to-l after:from-[var(--ui-bg)]/80 via-[var(--ui-bg)]/40 after:to-[var(--ui-bg)]/10 before:-left-0.5 after:-right-0.5"
+// const embla_before_after = "before:bg-linear-to-r before:from-[var(--ui-bg)]/65 before:to-[var(--ui-bg)] after:bg-linear-to-l after:from-[var(--ui-bg)]/65 after:to-[var(--ui-bg)] before:-left-0.5 after:-right-0.5"
 </script>
 
 <template>
-  <div class="embla max-w-sm mx-auto">
+  <div
+    class="embla relative max-w-sm sm:max-w-md md:max-w-lg mx-auto max-h-fit"
+    :class="[embla_before_after]"
+  >
+    <!-- <USeparator
+      orientation="vertical"
+      size="lg"
+      color="primary"
+      :class="[local_modelValue%10==0 ? 'h-28' : 'h-14']"
+      class="absolute top-0 left-[calc(50%-0px)] right-[calc(50%-0px)] z-1"
+    /> -->
+
     <div
       ref="emblaRef"
-      class="embla__viewport overflow-hidden"
+      class="embla__viewport overflow-hidden w-full relative"
     >
       <div
         class="embla__container flex touch-pan-y touch-pinch-zoom"
@@ -72,10 +93,10 @@ defineExpose({
               orientation="vertical"
               :label="value%10===0 ? `${value}` : ''"
               :ui="{
-                container: ['order-last', value%10 !== 0 && 'opacity-0'].join(' '),
+                container: ['order-last', value % 10 !== 0 && 'opacity-0'].join(' '),
+                border: value==local_modelValue ? 'border-white' : '',
               }"
-              :size="value==local_modelValue ? 'md' : 'xs'"
-              :color="value==local_modelValue ? 'primary' : 'neutral'"
+              :size="value==local_modelValue ? 'lg' : 'sm'"
               :class="[value%10==0 ? 'h-28' : 'h-14']"
             />
           </div>
@@ -86,5 +107,15 @@ defineExpose({
 </template>
 
 <style scoped>
-
+.embla:before,
+.embla:after {
+  position:absolute;
+  top: 0;
+  bottom: 0;
+  /* content: ''; */
+  display: block;
+  /* width: calc(50%-20px); */
+  z-index: 1;
+  pointer-events: none;
+}
 </style>
