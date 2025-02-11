093ce43818a62fab5291318ef983847b431fe0a2
Cybandy <andyboat75@gmail.com>
2025-02-11 07:58:08 +0100

    added mobile templates

---
+++ components/base/template/menu/mobileV1.vue
@@ -0,0 +1,102 @@
+<script setup lang='ts'>
+import type { PropType } from 'vue';
+import type { NavigationMenuItem, NavigationMenuProps } from '#ui/types'
+import type { MaybeArrayOfArray } from '#ui/types/utils';
+
+const props = defineProps({
+  menu: {
+    type: Object as PropType<NavigationMenuProps<MaybeArrayOfArray<NavigationMenuItem>>>,
+    default: () => ({
+      items: [
+        {
+          label: 'Home',
+          icon: 'i-solar-home-angle-bold',
+          to: '/'
+        },
+        {
+          label: 'Calendar',
+          icon: 'i-solar-calendar-outline'
+        },
+        {
+          label: 'Tracker',
+          icon: 'i-solar-graph-new-outline'
+        },
+        {
+          label: 'Articles',
+          icon: 'i-solar-document-text-outline'
+        },
+        {
+          label: 'Account',
+          icon: 'i-solar-user-linear'
+        },
+      ],
+      ui: {
+        link: 'flex-col justify-center text-xs',
+        linkLeadingIcon: 'size-6',
+        root: 'w-full',
+        list: 'gap-5',
+      }
+    })
+  },
+  // items: {
+  //   type: Array as PropType<MaybeArrayOfArray<NavigationMenuItem>>,
+  //   default: () => ([
+  //     {
+  //       label: 'Home',
+  //       icon: 'i-solar-home-angle-bold',
+  //       to: '/'
+  //     },
+  //     {
+  //       label: 'Calendar',
+  //       icon: 'i-solar-calendar-outline'
+  //     },
+  //     {
+  //       label: 'Tracker',
+  //       icon: 'i-solar-graph-new-outline'
+  //     },
+  //     {
+  //       label: 'Articles',
+  //       icon: 'i-solar-document-text-outline'
+  //     },
+  //     {
+  //       label: 'Account',
+  //       icon: 'i-solar-user-linear'
+  //     },
+  //   ])
+  // },
+
+})
+
+// const items = [
+//   {
+//     label: 'Home',
+//     icon: 'i-solar-home-angle-bold',
+//     to: '/'
+//   },
+//   {
+//     label: 'Calendar',
+//     icon: 'i-solar-calendar-outline'
+//   },
+//   {
+//     label: 'Tracker',
+//     icon: 'i-solar-graph-new-outline'
+//   },
+//   {
+//     label: 'Articles',
+//     icon: 'i-solar-document-text-outline'
+//   },
+//   {
+//     label: 'Account',
+//     icon: 'i-solar-user-linear'
+//   },
+// ]
+</script>
+
+<template>
+  <UNavigationMenu
+    :items="menu.items"
+    class="w-full justify-center"
+    :variant="menu.variant"
+    :ui="menu.ui"
+  />
+</template>
---
+++ components/base/template/mobile/mobileV1.vue
@@ -0,0 +1,100 @@
+<script setup lang='ts'>
+import type { PropType } from 'vue'
+import type { DropdownMenuItem, ButtonProps, DropdownMenuProps } from '#ui/types'
+
+const menuEl = useTemplateRef('menuEl')
+const { width: menu_width, height: menu_height } = useElementSize(menuEl)
+
+const props = defineProps({
+  menuButton: {
+    type: Object as PropType<ButtonProps>,
+    default: () => ({
+      color: 'neutral',
+      variant: 'link',
+      icon: 'i-heroicons-ellipsis-vertical'
+    })
+  },
+  menu: {
+    type: Object as PropType<DropdownMenuProps<DropdownMenuItem>>,
+    default: () => ({
+      items: [
+        [{
+          label: 'Notification',
+          icon: 'i-solar-bell-bing-linear'
+        },
+        {
+          label: 'Billing',
+          icon: 'i-solar-card-2-outline'
+        },
+        {
+          label: 'Settings',
+          icon: 'i-solar-settings-outline'
+        },],
+        [
+          {
+            label: 'Logout',
+            icon: 'i-solar-logout-3-linear'
+          }
+        ]
+      ],
+      content: {
+        align: 'end',
+        side: 'bottom'
+      },
+      ui: { content: 'w-48' }
+    })
+  },
+  name: {
+    type: String,
+    default: 'QB Tech'
+  }
+})
+
+</script>
+
+<template>
+  <UContainer class="h-dvh max-h-dvh max-w-[var(--app-width)] mx-auto overflow-auto relative">
+    <div
+      class="w-full overflow-x-hidden overflow-y-auto relative flex flex-col space-y-2"
+      :style="`height: calc(100% - ${menu_height}px)`"
+    >
+      <div class="flex items-center justify-between py-4">
+        <slot name="logo">
+          <p class="text-lg">
+            QB Tech
+          </p>
+        </slot>
+
+        <slot name="appName">
+          <p class="text-highlighted font-medium">
+            {{ name }}
+          </p>
+        </slot>
+
+        <slot name="menu">
+          <UDropdownMenu
+            v-bind="props.menu"
+            :content="{
+              align: 'end',
+              side: 'bottom',
+            }"
+            :ui="{ content: 'w-48' }"
+          >
+            <UButton v-bind="props.menuButton" />
+          </UDropdownMenu>
+        </slot>
+      </div>
+      <div class="w-full flex-grow overflow-y-auto">
+        <slot />
+      </div>
+    </div>
+    <div
+      ref="menuEl"
+      class="w-full h-fit absolute right-0 left-0 bottom-0"
+    >
+      <slot name="footerMenu">
+        <BaseTemplateMenuMobileV1 />
+      </slot>
+    </div>
+  </UContainer>
+</template>
